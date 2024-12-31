cf90a4daff275ba4de25a48975f64296594b412c
Clarissa Bida Dennis <83540@siswa.unimas.my>
2024-12-31 22:35:47 +0800

    Delete Teacher.js

--- Teacher.js
+++
@@ -1,9 +0,0 @@
-function showContent(type) {
-    const mainDisplay = document.getElementById('main-display');
-
-    if (type === 'learning-material') {
-        mainDisplay.innerHTML = '<h2>Learning Material</h2><p>List of learning materials will be displayed here.</p>';
-    } else if (type === 'student-data') {
-        mainDisplay.innerHTML = '<h2>Student Data Management</h2><p>Manage student data here.</p>';
-    }
-}
